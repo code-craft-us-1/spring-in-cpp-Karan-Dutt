dd8cd064c7abcb4b58a7b6569c7d63cc216e8670
Karan Dutt <karan.dutt@philips.com>
2024-08-27 20:34:04 +0530

    Updated the stats .h and .cpp file

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,14 @@
 #include "stats.h"
+#include <vector>
+#include <numeric>
+#include <algorithm> 
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& values) 
+{
+    Statistics::Stats statsValue;
+    statsValue.average = std::accumulate( values.begin(), values.end(), 0.0) / values.size();
+    statsValue.min = *std::min_element(values.begin(), values.end());
+    statsValue.max = *std::max_element(values.begin(), values.end());
+    return statsValue;
     //Implement statistics here
 }
--- stats.h
+++ stats.h
@@ -2,6 +2,12 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    struct Stats
+    {
+        double average = NAN;
+        double max = NAN;
+        double min = NAN;
+
+    };
+    Stats ComputeStatistics(const std::vector<double>& );
 }
